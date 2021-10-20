72581fa0cd04f95d238cf3e4887c6a795a0e6514
sboamah <sboamah@stevens.edu>
2021-10-20 13:03:35 -0400

    Add color constants

---
+++ lib/constants/colorConstants.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const buttonColor = Color(0x30658C); 
+  static const formField = Color(0xE0E0E0);
+  static const bodyText = Colors.black;
+  static const hintText = Color(0x8F8F8F);
+  static const buttonText = Colors.white;
+  static const appBar = Color(0x5498FF);
+  static const unfavoredButton = Color(0x8C3030);
+}
