1940443ffde8c058f0f890ae8e69522ea17671f1
Leena Domadia <leena.domadia2018@gmail.com>
2021-10-22 11:08:49 -0400

    added sign up and login buttons

--- lib/startup.dart
+++ lib/startup.dart
@@ -21,23 +21,50 @@ class _StartUpPageState extends State<StartUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-            child: Column(children: const <Widget>[
-          Padding(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const Padding(
               padding: EdgeInsets.only(top: 320.0),
               child: Center(
-                  child: Text(
-                'ReCollect',
-                style: TextStyle(fontSize: 64.0, fontWeight: FontWeight.w900),
-              ))),
-          Padding(
-              padding: EdgeInsets.only(top: 450.0),
-              child: Center(
-                child: ElevatedButton(onPressed: () {}, child: Text('Login')),
-              ))
-        ])));
+                child: Text(
+                  'ReCollect',
+                  style: TextStyle(fontSize: 64.0, fontWeight: FontWeight.w900),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 68.0),
+              height: 68,
+              width: 230,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextButton(
+                child: const Text('Sign Up',
+                    style: TextStyle(color: Colors.white, fontSize: 24.0)),
+                onPressed: () {},
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 22.0),
+              height: 68,
+              width: 230,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextButton(
+                child: const Text('Login',
+                    style: TextStyle(color: Colors.white, fontSize: 24.0)),
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
-
-void run() => {};
