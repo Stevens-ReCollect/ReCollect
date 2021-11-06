c845e11d8a8a2b6d911cd95fac7960c70f9fb0d1
Joshua <jashsmash10@gmail.com>
2021-11-06 14:12:07 -0400

    got android to work, and downloaded the app on phone

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -40,9 +41,7 @@ class MyApp extends StatelessWidget {
                   child: CircularProgressIndicator(),
                 );
               }
-            }
-          )
-        );
+            }));
   }
 }
 
