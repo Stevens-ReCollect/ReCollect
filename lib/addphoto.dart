916db20f2978a63b1ea9061794300f3985ee51e1
Leena Domadia <leena.domadia2018@gmail.com>
2021-11-19 12:37:53 -0500

    added photo icon

--- lib/addphoto.dart
+++ lib/addphoto.dart
@@ -4,6 +4,8 @@ import 'package:recollect_app/signup.dart';
 
 import 'constants/routeConstants.dart';
 
+void main() => runApp(AddPhotoPage());
+
 class AddPhotoPage extends StatefulWidget {
   @override
   _AddPhotoPageState createState() => _AddPhotoPageState();
@@ -14,91 +16,128 @@ class _AddPhotoPageState extends State<AddPhotoPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.only(top: 50.0, left: 10.0),
-              alignment: Alignment.centerLeft,
-              child: IconButton(
-                icon: const Icon(Icons.arrow_back_ios_new),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 20.0),
-              child: const Center(
-                child: Text(
-                  '{Selected Photo}',
-                  style: TextStyle(
-                    fontSize: 25.0,
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w900,
-                  ),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 50.0, left: 10.0),
+                alignment: Alignment.centerLeft,
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back_ios_new),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                 ),
               ),
-            ),
-            Container(
-              height: 250.0,
-              width: 325.0,
-              margin: const EdgeInsets.only(top: 15.0, left: 0.0),
-              child: const TextField(
-                maxLines: 15,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Description',
-                  hintText: 'Desciption',
-                  alignLabelWithHint: true,
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 20.0),
-              child: const Center(
-                child: Text(
-                  'Add tags',
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w900,
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 250.0, left: 0.0),
-              height: 68,
-              width: 230,
-              child: TextButton(
-                child: const Text(
-                  'Save',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      ColorConstants.buttonColor),
-                  foregroundColor: MaterialStateProperty.all<Color>(
-                      ColorConstants.buttonText),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
+              ListTile(
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(5.0),
+                  child: Container(
+                    height: 60,
+                    width: 60,
+                    child: Image.network(
+                      'https://www.brides.com/thmb/1bR5-1Y1y0drTsbS8fhu3gYJxBQ=/1425x0/filters:no_upscale():max_bytes(200000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__brides__public__brides-services__production__2018__12__03__5c057f05648d6b2dd3b5a13a_kristen-and-jonathan-wedding22-fd1d0dc5dfa94482a9c3273b663c4a2d.jpg',
+                      fit: BoxFit.fill,
                     ),
                   ),
                 ),
-                onPressed: () {
-                  Navigator.pushNamed(context, RouteConstants.memoryHomeRoute);
-                },
+                title: const Text(
+                  'Photo Selected',
+                  style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18.0),
+                ),
+                subtitle: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Change Photo',
+                    style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 12.0),
+                  ),
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    alignment: Alignment.topLeft,
+                  ),
+                ),
               ),
-            ),
-          ],
+              // Container(
+              //   margin: const EdgeInsets.only(top: 20.0),
+              //   child: const Center(
+              //     child: Text(
+              //       '{Selected Photo}',
+              //       style: TextStyle(
+              //         fontSize: 25.0,
+              //         fontFamily: 'Roboto',
+              //         fontWeight: FontWeight.w900,
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              Container(
+                height: 115.0,
+                width: 360.0,
+                margin: const EdgeInsets.only(top: 15.0, left: 0.0),
+                child: const TextField(
+                  maxLines: 15,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Description',
+                    hintText: 'Desciption',
+                    alignLabelWithHint: true,
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                child: const Center(
+                  child: Text(
+                    'Add tags',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 250.0, left: 0.0),
+                height: 68,
+                width: 230,
+                child: TextButton(
+                  child: const Text(
+                    'Save',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        ColorConstants.buttonColor),
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                        ColorConstants.buttonText),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(
+                        context, RouteConstants.memoryHomeRoute);
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
