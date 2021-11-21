7528fad9dded438cd91d6d9d19d15736f02f6498
Leena Domadia <leena.domadia2018@gmail.com>
2021-11-20 21:27:35 -0500

    added unit test

--- test/addphoto_test.dart
+++ test/addphoto_test.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:recollect_app/login.dart';
+import 'package:recollect_app/addphoto.dart';
 
 void main() {
-  testWidgets('Log In screen loads correctly', (WidgetTester tester) async {
+  testWidgets('Add Photo screen loads correctly', (WidgetTester tester) async {
     // Build the app
     Widget testWidget = MediaQuery(
       data: const MediaQueryData(),
       child: MaterialApp(
-        home: LoginPage(),
+        home: AddPhotoPage(),
       ),
     );
     await tester.pumpWidget(testWidget);
 
-    // Verify app title appears
-    expect(find.text('Log In'), findsNWidgets(2));
+    // Verify page title appears
+    expect(find.text('Photo Selected'), findsNWidgets(1));
 
-    // Verify Login and Sign Up button appears
+    // Verify Change Photo and Save button appears
     expect(find.byType(TextButton), findsNWidgets(2));
 
-    // Verify TextField appears
-    expect(find.byType(TextField), findsNWidgets(2));
+    // Verify Description appears
+    expect(find.byType(TextField), findsNWidgets(1));
     // Need to test colors are correct
   });
 }
