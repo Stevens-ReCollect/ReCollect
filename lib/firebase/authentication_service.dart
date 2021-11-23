df51707e38252a4e977217e7b6541ff84ff66bba
t_hectjo <joshua.j.hector@autodesk.com>
2021-11-22 23:02:04 -0500

    fixed auth service file

--- lib/firebase/authentication_service.dart
+++ lib/firebase/authentication_service.dart
@@ -7,23 +7,23 @@ class AuthenticationService {
 
   Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
 
-  Future<String> signIn({String email, String password}) async {
+  Future<String> signIn({required String email, required String password}) async {
     try {
       await _firebaseAuth.signInWithEmailAndPassword(
           email: email, password: password);
       return "Signed In";
-    } on FirebaseAuthException catch (e) {
-      return e.message.toString();
+    } on FirebaseAuthException catch (ex) {
+      return ex.message.toString();
     }
   }
 
-  Future<String> signUp({String email, String password}) async {
+  Future<String> signUp({required String email, required String password}) async {
     try {
       await _firebaseAuth.createUserWithEmailAndPassword(
           email: email, password: password);
       return "Signed Up";
-    } on FirebaseAuthException catch (e) {
-      return e.message.toString();
+    } on FirebaseAuthException catch (ex) {
+      return ex.message.toString();
     }
   }
 }
