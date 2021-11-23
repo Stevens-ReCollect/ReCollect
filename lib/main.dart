25df1d657e7b8d089e1def303517427ca6583857
t_hectjo <joshua.j.hector@autodesk.com>
2021-11-22 22:40:28 -0500

    signin work

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:recollect_app/firebase/authentication_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
 import 'package:recollect_app/constants/colorConstants.dart';
 import 'package:recollect_app/constants/routeConstants.dart';
 import 'package:recollect_app/constants/textSizeConstants.dart';
@@ -25,12 +28,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ReCollect',
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) => context.read<AuthenticationService>().authStateChanges,
+        ),
+      ], 
+      child: MaterialApp(
+      title: 'ReCollect', 
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: AuthenticationWrapper(),
       routes: {
         RouteConstants.homeRoute: (context) => MyHomePage(),
         RouteConstants.memExRoute: (context) => MemoryPage(),
@@ -40,7 +52,20 @@ class MyApp extends StatelessWidget {
         RouteConstants.loginRoute: (context) => LoginPage(),
         RouteConstants.memoryHomeRoute: (context) => MemoryHomePage(),
       },
-    );
+    ),);
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  const AuthenticationWrapper({ Key? key }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User>();
+
+    if(firebaseUser != null) {
+      return MyHomePage();
+    }
+    return LoginPage();
   }
 }
 
@@ -51,23 +76,26 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => MyHomePageState();
 }
 
-
 class MyHomePageState extends State<MyHomePage> {
-static late int accountMode = 0;
-createNewMemory() {
+  static late int accountMode = 0;
+  createNewMemory() {
     //Create New Memory button toggle
-    if(accountMode == 0){
-    return Padding(
-      padding: EdgeInsets.only(bottom: 15),
-      child:ElevatedButton(onPressed:() {
-        Navigator.pushNamed(context, RouteConstants.createMemory);
-      }, 
-    style: ElevatedButton.styleFrom(padding: const EdgeInsets.all(15), primary: ColorConstants.buttonColor,
-                      textStyle: TextStyle(fontSize: TextSizeConstants.getadaptiveTextSize(context, TextSizeConstants.buttonText))
-    ),
-    child: Text('Create New Memory')));
-    } 
-    if(accountMode == 1) {
+    if (accountMode == 0) {
+      return Padding(
+          padding: EdgeInsets.only(bottom: 15),
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, RouteConstants.createMemory);
+              },
+              style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(15),
+                  primary: ColorConstants.buttonColor,
+                  textStyle: TextStyle(
+                      fontSize: TextSizeConstants.getadaptiveTextSize(
+                          context, TextSizeConstants.buttonText))),
+              child: Text('Create New Memory')));
+    }
+    if (accountMode == 1) {
       return SizedBox();
     }
   }
@@ -75,42 +103,54 @@ createNewMemory() {
   caregiverPin(BuildContext context) {
     //Caregiver Pin pop up
     return AlertDialog(
-    title: Text('Enter Caregiver Pin before entering Edit Mode.', 
-    style: TextStyle(fontSize: TextSizeConstants.getadaptiveTextSize(context, TextSizeConstants.bodyText),)),
-    content: Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        TextField(
-          obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter pin',
-                  labelStyle: TextStyle(fontSize: TextSizeConstants.getadaptiveTextSize(context, TextSizeConstants.formField)),
-                  hintText: '####',
-                  hintStyle: TextStyle(fontSize: TextSizeConstants.getadaptiveTextSize(context, TextSizeConstants.formField)),
-                ),
+        title: Text('Enter Caregiver Pin before entering Edit Mode.',
+            style: TextStyle(
+              fontSize: TextSizeConstants.getadaptiveTextSize(
+                  context, TextSizeConstants.bodyText),
+            )),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter pin',
+                labelStyle: TextStyle(
+                    fontSize: TextSizeConstants.getadaptiveTextSize(
+                        context, TextSizeConstants.formField)),
+                hintText: '####',
+                hintStyle: TextStyle(
+                    fontSize: TextSizeConstants.getadaptiveTextSize(
+                        context, TextSizeConstants.formField)),
               ),
-      ],
-    ),
-    actions: <Widget>[
-      ElevatedButton(
-        style: ElevatedButton.styleFrom(padding: EdgeInsets.all(15), primary: ColorConstants.buttonColor),
-        onPressed: () {
-          accountMode = 0;
-          
-          Navigator.of(context).pop();
-        },
-        child: Text('Continue', style: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(context, TextSizeConstants.buttonText))),
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(15),
+                primary: ColorConstants.buttonColor),
+            onPressed: () {
+              accountMode = 0;
 
-      )]);
+              Navigator.of(context).pop();
+            },
+            child: Text('Continue',
+                style: TextStyle(
+                    fontSize: 0.7 *
+                        TextSizeConstants.getadaptiveTextSize(
+                            context, TextSizeConstants.buttonText))),
+          )
+        ]);
   }
 
   @override
   void initState() {
-      toggleColors(accountMode);
-      super.initState();
-    
+    toggleColors(accountMode);
+    super.initState();
   }
 
   @override
@@ -119,132 +159,151 @@ createNewMemory() {
     var deviceWidth = queryData.size.width;
     var deviceHeight = queryData.size.height;
     return Scaffold(
-      appBar: AppBar(
-        // App bar properties
-        // title: Text(widget.title),
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-        leadingWidth: 300,
-        backgroundColor: ColorConstants.appBar,
-        actions: <Widget>[
-          Row(
-            children: <Widget> [
-         Listener(
-    // onPointerDown: ColorConstants().toggleColors(value),
-    child: ToggleSwitch( //Toggle between modes
-          minWidth: 0.3*deviceWidth,
-          changeOnTap: true,
-          inactiveBgColor: Colors.white,
-          dividerColor: Colors.black,
-          activeBgColor: [ColorConstants.buttonColor], //toggle colors stuck :(
-          initialLabelIndex: accountMode,
-          fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(context, TextSizeConstants.buttonText),
-          totalSwitches: 2,
-          labels: const ['Edit Mode', 'Story Mode'],
-          onToggle: (value) {   
-              //  print('switched to: $value');
-               toggleColors(value);
-               accountMode = value;
-               if (accountMode == 0) {
-                 showDialog(
-              context: context,
-              builder: (BuildContext context) => caregiverPin(context),
-                 );}
-  
-          },         
-        
-  )),
-      //   SizedBox(
-      //    width: 0.38*deviceWidth,
-      //  ),
-        createSettings(),
-        ],),
-        ]
-      ),
-      body: SingleChildScrollView(
-        child: AspectRatio(
-        aspectRatio: 100 / 100,
-        child: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Container(
-            padding: EdgeInsets.all(20),
-            child: Text(
-              'New Memories', style: TextStyle(color: ColorConstants.bodyText, fontSize: TextSizeConstants.getadaptiveTextSize(context, TextSizeConstants.h2)),
-            )),
-            createNewMemory(),
-            InkWell(
-            onTap: () {
-              Navigator.pushNamed(context, RouteConstants.memExRoute);
-            }, 
-           child: Stack(
-             alignment: Alignment.center,
-               children: <Widget>[
-                 Container(
-                   width: 0.8*deviceWidth,
-                   height: deviceHeight / 4,
-                decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                image: DecorationImage(
-                fit: BoxFit.cover,
-                alignment: Alignment.topLeft, 
-                image: AssetImage('lib/images/wedding-placeholder.jpg'), 
-                ))),
-                Container(
-                alignment: Alignment.bottomLeft,
-                decoration: const BoxDecoration(color: Colors.black26, 
-                borderRadius: BorderRadius.all(Radius.circular(20)),),
-                width: 0.8*deviceWidth,
-                height: deviceHeight / 4,
-                padding: const EdgeInsets.only(left: 20, bottom: 10),
-                 child: Text('Wedding', 
-                 style: TextStyle(color: ColorConstants.buttonText, 
-                  fontSize: TextSizeConstants.getadaptiveTextSize(context, TextSizeConstants.buttonText), 
-                  fontWeight: FontWeight.w900), textAlign: TextAlign.left,),
-                )
-               ],
-             ) 
-            )
-  
-          ],
-        ),
-    ))));
+        appBar: AppBar(
+            // App bar properties
+            // title: Text(widget.title),
+            centerTitle: true,
+            automaticallyImplyLeading: false,
+            leadingWidth: 300,
+            backgroundColor: ColorConstants.appBar,
+            actions: <Widget>[
+              Row(
+                children: <Widget>[
+                  Listener(
+                      // onPointerDown: ColorConstants().toggleColors(value),
+                      child: ToggleSwitch(
+                    //Toggle between modes
+                    minWidth: 0.3 * deviceWidth,
+                    changeOnTap: true,
+                    inactiveBgColor: Colors.white,
+                    dividerColor: Colors.black,
+                    activeBgColor: [
+                      ColorConstants.buttonColor
+                    ], //toggle colors stuck :(
+                    initialLabelIndex: accountMode,
+                    fontSize: 0.7 *
+                        TextSizeConstants.getadaptiveTextSize(
+                            context, TextSizeConstants.buttonText),
+                    totalSwitches: 2,
+                    labels: const ['Edit Mode', 'Story Mode'],
+                    onToggle: (value) {
+                      //  print('switched to: $value');
+                      toggleColors(value);
+                      accountMode = value;
+                      if (accountMode == 0) {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) =>
+                              caregiverPin(context),
+                        );
+                      }
+                    },
+                  )),
+                  //   SizedBox(
+                  //    width: 0.38*deviceWidth,
+                  //  ),
+                  createSettings(),
+                ],
+              ),
+            ]),
+        body: SingleChildScrollView(
+            child: AspectRatio(
+                aspectRatio: 100 / 100,
+                child: Center(
+                  // Center is a layout widget. It takes a single child and positions it
+                  // in the middle of the parent.
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Container(
+                          padding: EdgeInsets.all(20),
+                          child: Text(
+                            'New Memories',
+                            style: TextStyle(
+                                color: ColorConstants.bodyText,
+                                fontSize: TextSizeConstants.getadaptiveTextSize(
+                                    context, TextSizeConstants.h2)),
+                          )),
+                      createNewMemory(),
+                      InkWell(
+                          onTap: () {
+                            Navigator.pushNamed(
+                                context, RouteConstants.memExRoute);
+                          },
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: <Widget>[
+                              Container(
+                                  width: 0.8 * deviceWidth,
+                                  height: deviceHeight / 4,
+                                  decoration: const BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(20)),
+                                      image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                        alignment: Alignment.topLeft,
+                                        image: AssetImage(
+                                            'lib/images/wedding-placeholder.jpg'),
+                                      ))),
+                              Container(
+                                alignment: Alignment.bottomLeft,
+                                decoration: const BoxDecoration(
+                                  color: Colors.black26,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(20)),
+                                ),
+                                width: 0.8 * deviceWidth,
+                                height: deviceHeight / 4,
+                                padding:
+                                    const EdgeInsets.only(left: 20, bottom: 10),
+                                child: Text(
+                                  'Wedding',
+                                  style: TextStyle(
+                                      color: ColorConstants.buttonText,
+                                      fontSize:
+                                          TextSizeConstants.getadaptiveTextSize(
+                                              context,
+                                              TextSizeConstants.buttonText),
+                                      fontWeight: FontWeight.w900),
+                                  textAlign: TextAlign.left,
+                                ),
+                              )
+                            ],
+                          ))
+                    ],
+                  ),
+                ))));
   }
-  
-toggleColors(int value) {
- 
-  if (value == 0) { //Attempt at toggle
-            ColorConstants.appBar = const Color(0xFF00CB5D);
-            ColorConstants.buttonColor = const Color(0xFF308C39);
-            }  
-      if (value == 1) {
-              ColorConstants.appBar = const Color(0xFF3065FC);
-              ColorConstants.buttonColor = const Color(0xFF30658C);
-            }
-          setState(() {
-            
-          });
 
+  toggleColors(int value) {
+    if (value == 0) {
+      //Attempt at toggle
+      ColorConstants.appBar = const Color(0xFF00CB5D);
+      ColorConstants.buttonColor = const Color(0xFF308C39);
+    }
+    if (value == 1) {
+      ColorConstants.appBar = const Color(0xFF3065FC);
+      ColorConstants.buttonColor = const Color(0xFF30658C);
+    }
+    setState(() {});
+  }
+
+  createSettings() {
+    if (accountMode == 0) {
+      //settings on toggle
+      return IconButton(
+        icon: Icon(Icons.settings),
+        iconSize: TextSizeConstants.getadaptiveTextSize(
+            context, TextSizeConstants.buttonText), //Settings Icon
+        onPressed: () {
+          Navigator.pushNamed(context, RouteConstants.progressRoute);
+        },
+      );
+    }
+    if (accountMode == 1) {
+      return SizedBox();
+    }
+  }
 }
-
-createSettings(){ 
-if(accountMode == 0){//settings on toggle
- return IconButton(
-                icon: Icon(Icons.settings),  
-                iconSize: TextSizeConstants.getadaptiveTextSize(context, TextSizeConstants.buttonText),//Settings Icon 
-                onPressed: () {  
-                  Navigator.pushNamed(context, RouteConstants.progressRoute);
-                },
-                );
-  } 
-  if(accountMode == 1){
-  return SizedBox();
-}
-
-  
-}}
