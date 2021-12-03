2ea390584f3f4d82320f515af3f1db4eae09fdca
Leena Domadia <leena.domadia2018@gmail.com>
2021-12-02 22:45:36 -0500

    updated addphoto

--- lib/firebase/firestore_service.dart
+++ lib/firebase/firestore_service.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:recollect_app/firebase/authentication_service.dart';
@@ -7,7 +9,7 @@ class FirestoreService {
 
   Future<void> addNewUser(
       {required String email, required String caregiverPin}) {
-    CollectionReference users = FirebaseFirestore.instance.collection('users');
+    CollectionReference users = _firestore.collection('users');
     User? currentUser = AuthenticationService().getUser();
     if (currentUser == null) {
       throw Exception('currentUser is null');
@@ -16,7 +18,7 @@ class FirestoreService {
         .add({
           'user_email': email,
           'admin_user_email': email,
-          'caregiver_pin': caregiverPin
+          'caregiver_pin': caregiverPin,
         })
         .then((value) => print("User Added"))
         .catchError((error) => print("Failed to add user: $error"));
@@ -27,22 +29,19 @@ class FirestoreService {
       required String startDate,
       String? endDate,
       String? description}) {
-    CollectionReference memories =
-        FirebaseFirestore.instance.collection('memories');
     User? currentUser = AuthenticationService().getUser();
     if (currentUser == null) {
       throw Exception('currentUser is null');
     }
-    return memories
-        .add({
-          'user_email': currentUser.email,
-          'title': title,
-          'start_date': startDate,
-          'end_date': endDate,
-          'description': description,
-          'moment_documents': []
-        })
-        .then((value) => print("Memory Added"))
-        .catchError((error) => print("Failed to add memory: $error"));
+    CollectionReference memories = _firestore.collection('memories');
+    return memories.add({
+      'user_email': currentUser.email,
+      'title': title,
+      'start_date': startDate,
+      'end_date': endDate,
+      'description': description,
+    }).then((value) {
+      print("Memory Added");
+    }).catchError((error) => print("Failed to add memory: $error"));
   }
 }
