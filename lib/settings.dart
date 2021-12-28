e47d0a880453bead777ca2f383457bafcd038096
sboamah <sboamah@stevens.edu>
2021-12-27 23:49:30 -0500

    Reattempting settings page but without settings package

--- lib/settings.dart
+++ lib/settings.dart
@@ -12,9 +12,11 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   var isHighContrast = false;
-
   @override
   Widget build(BuildContext context) {
+    MediaQueryData queryData = MediaQuery.of(context);
+    var deviceWidth = queryData.size.width;
+    var deviceHeight = queryData.size.height;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -22,91 +24,145 @@ class _SettingsPageState extends State<SettingsPage> {
         backgroundColor: ColorConstants.appBar,
         automaticallyImplyLeading: true,
         ),
-      body: SettingsList(
-        contentPadding: const EdgeInsets.all(10),
-        sections: [
-          SettingsSection(
-            titlePadding: const EdgeInsets.all(10),
-            subtitlePadding: const EdgeInsets.all(10),
-            title: 'Account Settings',
-            titleTextStyle: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
-                  context, TextSizeConstants.bodyText),),
-            tiles: [
-              SettingsTile(
-                title: 'Change Password',
-                titleTextStyle: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
-                  context, TextSizeConstants.bodyText),),
-                leading: const Icon(Icons.lock_outline),
-                onPressed: (BuildContext context) {},
-              ),]),
-
-          SettingsSection(
-            titlePadding: const EdgeInsets.all(10),
-            subtitlePadding: const EdgeInsets.all(10),
-            title: 'Accessibility',
-            titleTextStyle: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
-                  context, TextSizeConstants.bodyText),),
-            tiles: [
-              SettingsTile(
-                title: 'Language',
-                titleTextStyle: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
-                  context, TextSizeConstants.bodyText),),
-                subtitle: 'English',
-                subtitleTextStyle: TextStyle(fontSize: 0.6*TextSizeConstants.getadaptiveTextSize(
-                  context, TextSizeConstants.bodyText),),
-                leading: const Icon(Icons.language),
-                onPressed: (BuildContext context) {},
-              ),
-              SettingsTile(
-                title: 'Progress Report',
-                titleTextStyle: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
-                  context, TextSizeConstants.bodyText),),
-                subtitle: "View your loved one's progress",
-                subtitleTextStyle: TextStyle(fontSize: 0.6*TextSizeConstants.getadaptiveTextSize(
-                  context, TextSizeConstants.bodyText),),
-                leading: const Icon(Icons.grade),
-                onPressed: (BuildContext context) {
-                  Navigator.pushNamed(context, RouteConstants.progressRoute);
-                },
-              ),
-              SettingsTile.switchTile(
-                title: 'High Contrast',
-                switchActiveColor: ColorConstants.appBar,
-                titleTextStyle: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
-                  context, TextSizeConstants.bodyText),),
-                leading: const Icon(Icons.color_lens),
-                switchValue: isHighContrast,
-                onToggle: (bool value) {
-                  isHighContrast = value;
-                  if(isHighContrast == true){
-                    //TODO: Figure way to send to settings on operating system
-                  }
-                  // setState(() {
-                    
-                  // });
-                },
-              ),
-              SettingsTile(
-                title: 'Text Size Adjustment',
-                titleTextStyle: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
-                  context, TextSizeConstants.bodyText),),
-                leading: const Icon(Icons.text_fields),
-                onPressed: (BuildContext context) {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const TextAdjustPage()));
-                },
-              ),
-              SettingsTile(
-                title: 'Log out',
-                titleTextStyle: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
-                  context, TextSizeConstants.bodyText),),
-                leading: const Icon(Icons.logout),
-                onPressed: (BuildContext context) {
-                  
-                },
-              ),
-            ],
-          ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: deviceWidth,
+            padding: const EdgeInsets.all(10),
+            child:Text('Account Settings', style: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+                  context, TextSizeConstants.bodyText), fontWeight: FontWeight.w700))),
+            Container(
+            width: deviceWidth,
+            padding: const EdgeInsets.all(20),
+            color: ColorConstants.formField,
+            child:InkWell(
+              child:Text('Change Password', style: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+                  context, TextSizeConstants.bodyText))),
+              onTap: (){},
+            )),
+            Container(
+            width: deviceWidth,
+            padding: const EdgeInsets.all(10),
+            child:Text('Assessibility', style: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+                  context, TextSizeConstants.bodyText), fontWeight: FontWeight.w700))),
+            Container(
+            width: deviceWidth,
+            padding: const EdgeInsets.all(20),
+            color: ColorConstants.formField,
+            child:InkWell(
+              child:Text('Language', style: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+                  context, TextSizeConstants.bodyText))),
+              onTap: (){},
+            )),
+            Container(
+            width: deviceWidth,
+            padding: const EdgeInsets.all(20),
+            color: ColorConstants.formField,
+            child:InkWell(
+              child:Text('Progress Report', style: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+                  context, TextSizeConstants.bodyText))),
+              onTap: (){},
+            )),
+            Container(
+            width: deviceWidth,
+            padding: const EdgeInsets.all(20),
+            color: ColorConstants.formField,
+            child: InkWell(
+              child:Text('High Contrast', style: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+                  context, TextSizeConstants.bodyText))),
+              onTap: (){},
+            )),
+            Container(
+            width: deviceWidth,
+            padding: const EdgeInsets.all(20),
+            color: ColorConstants.formField,
+            child: Row(
+            children:<Widget>[
+            IconButton(
+              icon:const Icon(Icons.text_fields), 
+              onPressed: () {  },),
+            InkResponse(
+              child:Text('Text Size Adjustment', style: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+                  context, TextSizeConstants.bodyText))),
+              onTap: () {}
+            )])),
+            Container(
+            width: deviceWidth,
+            padding: const EdgeInsets.all(20),
+            color: ColorConstants.formField,
+            child:InkWell(
+              child:Text('Log Out', style: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+                  context, TextSizeConstants.bodyText))),
+              onTap: (){},
+            )),
         ]));
+        //   SettingsSection(
+        //     titlePadding: const EdgeInsets.all(10),
+        //     subtitlePadding: const EdgeInsets.all(10),
+        //     title: 'Accessibility',
+        //     titleTextStyle: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+        //           context, TextSizeConstants.bodyText),),
+        //     tiles: [
+        //       SettingsTile(
+        //         title: 'Language',
+        //         titleTextStyle: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+        //           context, TextSizeConstants.bodyText),),
+        //         subtitle: 'English',
+        //         subtitleTextStyle: TextStyle(fontSize: 0.6*TextSizeConstants.getadaptiveTextSize(
+        //           context, TextSizeConstants.bodyText),),
+        //         leading: const Icon(Icons.language),
+        //         onPressed: (BuildContext context) {},
+        //       ),
+        //       SettingsTile(
+        //         title: 'Progress Report',
+        //         titleTextStyle: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+        //           context, TextSizeConstants.bodyText),),
+        //         subtitle: "View your loved one's progress",
+        //         subtitleTextStyle: TextStyle(fontSize: 0.6*TextSizeConstants.getadaptiveTextSize(
+        //           context, TextSizeConstants.bodyText),),
+        //         leading: const Icon(Icons.grade),
+        //         onPressed: (BuildContext context) {
+        //           Navigator.pushNamed(context, RouteConstants.progressRoute);
+        //         },
+        //       ),
+        //       SettingsTile.switchTile(
+        //         title: 'High Contrast',
+        //         switchActiveColor: ColorConstants.appBar,
+        //         titleTextStyle: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+        //           context, TextSizeConstants.bodyText),),
+        //         leading: const Icon(Icons.color_lens),
+        //         switchValue: isHighContrast,
+        //         onToggle: (bool value) {
+        //           isHighContrast = value;
+        //           if(isHighContrast == true){
+        //             //TODO: Figure way to send to settings on operating system
+        //           }
+        //           // setState(() {
+                    
+        //           // });
+        //         },
+        //       ),
+        //       SettingsTile(
+        //         title: 'Text Size Adjustment',
+        //         titleTextStyle: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+        //           context, TextSizeConstants.bodyText),),
+        //         leading: const Icon(Icons.text_fields),
+        //         onPressed: (BuildContext context) {
+        //           Navigator.push(context, MaterialPageRoute(builder: (context) => const TextAdjustPage()));
+        //         },
+        //       ),
+        //       SettingsTile(
+        //         title: 'Log out',
+        //         titleTextStyle: TextStyle(fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+        //           context, TextSizeConstants.bodyText),),
+        //         leading: const Icon(Icons.logout),
+        //         onPressed: (BuildContext context) {
+                  
+        //         },
+        //       ),
+        //     ],
+        //   ),
+        // ]));
   }
 }
 
