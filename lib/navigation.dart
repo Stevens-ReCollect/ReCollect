5a6b410ae7a09268aabb67cda4adfa40007ea6db
Kushal Patel <kushal1223@gmail.com>
2022-01-04 03:16:24 -0500

    Persistant nav bar if needed

--- lib/navigation.dart
+++ lib/navigation.dart
@@ -54,3 +54,68 @@ class NavigateState extends State<Navigate> {
         ),
       );
 }
+
+//PersistentTabController _controller = PersistentTabController(initialIndex: 0);
+
+/*class Navigate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PersistentTabView(
+      context,
+      controller: _controller,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      confineInSafeArea: true,
+      backgroundColor: Colors.white, // Default is Colors.white.
+      handleAndroidBackButtonPress: true, // Default is true.
+      resizeToAvoidBottomInset:
+          true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+      stateManagement: true, // Default is true.
+      hideNavigationBarWhenKeyboardShows:
+          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        colorBehindNavBar: Colors.white,
+      ),
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: ItemAnimationProperties(
+        // Navigation Bar's items animation properties.
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: ScreenTransitionAnimation(
+        // Screen transition animation on change of selected tab.
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle:
+          NavBarStyle.style1, // Choose the nav bar style with this property.
+    );
+  }
+}
+
+List<Widget> _buildScreens() {
+  return [MyHomePage(), ProgressReport(), SettingsPage()];
+}
+
+List<PersistentBottomNavBarItem> _navBarsItems() {
+  return [
+    PersistentBottomNavBarItem(
+      icon: Icon(Icons.home),
+      title: ("Home"),
+    ),
+    PersistentBottomNavBarItem(
+      icon: Icon(Icons.poll),
+      title: ("Progress Report"),
+    ),
+    PersistentBottomNavBarItem(
+        icon: Icon(Icons.settings),
+        title: ("Settings"),
+        onPressed: (context) {
+          pushNewScreen(context!, screen: SettingsPage(), withNavBar: true);
+        }),
+  ];
+}
+*/
