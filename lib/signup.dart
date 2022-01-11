4e98d9686fbdb31b5d98058dfbd8664d422fa194
Leena Domadia <leena.domadia2018@gmail.com>
2022-01-11 12:09:17 -0500

    changes to signup

--- lib/signup.dart
+++ lib/signup.dart
@@ -19,7 +19,20 @@ class _SignUpPageState extends State<SignUpPage> {
   final TextEditingController _password = TextEditingController();
   final TextEditingController _confirmPassword = TextEditingController();
   final TextEditingController _caregiverPin = TextEditingController();
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
   String signUpResult = "";
+
+  String? validateConfirmPassword(String? confirmPassword) {
+    if (confirmPassword == null || confirmPassword.isEmpty) {
+      return 'Confirm Password is required.';
+    }
+
+    if (confirmPassword != _password.text) {
+      return 'Passwords do not match.';
+    }
+
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,212 +40,221 @@ class _SignUpPageState extends State<SignUpPage> {
     var deviceWidth = queryData.size.width;
     var deviceHeight = queryData.size.height;
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.only(top: 50.0, left: 10.0),
-                alignment: Alignment.centerLeft,
-                child: IconButton(
-                  icon: const Icon(Icons.arrow_back_ios_new),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ),
-              Container(
-                width: 0.8 * deviceWidth,
-                margin: const EdgeInsets.only(top: 50.0, left: 0.0),
-                child: Text(
-                  'Sign Up',
-                  style: TextStyle(
-                    fontSize: TextSizeConstants.getadaptiveTextSize(
-                        context, TextSizeConstants.memoryTitle),
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w700,
+      home: Form(
+        key: _key,
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(top: 50.0, left: 10.0),
+                  alignment: Alignment.centerLeft,
+                  child: IconButton(
+                    icon: const Icon(Icons.arrow_back_ios_new),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
                   ),
                 ),
-              ),
-              Container(
-                width: 0.8 * deviceWidth,
-                margin: const EdgeInsets.only(top: 15.0, left: 0.0),
-                child: TextButton(
-                  child: RichText(
-                    text: TextSpan(
-                      text: 'Have an account? ',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: TextSizeConstants.getadaptiveTextSize(
-                            context, TextSizeConstants.bodyText),
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w400,
-                      ),
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: 'Log in.',
-                          style: TextStyle(
-                            color: ColorConstants.bodyText,
-                            fontSize: TextSizeConstants.getadaptiveTextSize(
-                                context, TextSizeConstants.bodyText),
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.w400,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.zero,
-                    alignment: Alignment.centerLeft,
-                  ),
-                  onPressed: () {
-                    Navigator.pushNamed(context, RouteConstants.loginRoute);
-                  },
-                ),
-              ),
-              Container(
-                width: 0.8 * deviceWidth,
-                margin: const EdgeInsets.only(top: 10.0, left: 0.0),
-                child: Text(
-                  signUpResult,
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: TextSizeConstants.getadaptiveTextSize(
-                        context, TextSizeConstants.hint),
-                  ),
-                ),
-              ),
-              Container(
-                width: 0.8 * deviceWidth,
-                margin: const EdgeInsets.only(top: 15.0, left: 0.0),
-                child: TextField(
-                  controller: _email,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                    labelStyle: TextStyle(
-                        fontSize: TextSizeConstants.getadaptiveTextSize(
-                            context, TextSizeConstants.formField)),
-                    hintText: 'example@example.com',
-                    hintStyle: TextStyle(
-                        fontSize: TextSizeConstants.getadaptiveTextSize(
-                            context, TextSizeConstants.formField)),
-                  ),
-                ),
-              ),
-              Container(
-                width: 0.8 * deviceWidth,
-                margin: const EdgeInsets.only(top: 15.0, left: 0.0),
-                child: TextField(
-                  obscureText: true,
-                  controller: _password,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    labelStyle: TextStyle(
-                        fontSize: TextSizeConstants.getadaptiveTextSize(
-                            context, TextSizeConstants.formField)),
-                  ),
-                ),
-              ),
-              Container(
-                width: 0.8 * deviceWidth,
-                margin: const EdgeInsets.only(top: 15.0, left: 0.0),
-                child: TextField(
-                  obscureText: true,
-                  controller: _confirmPassword,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Confirm Password',
-                    labelStyle: TextStyle(
-                        fontSize: TextSizeConstants.getadaptiveTextSize(
-                            context, TextSizeConstants.formField)),
-                  ),
-                ),
-              ),
-              Container(
-                width: 0.8 * deviceWidth,
-                margin: const EdgeInsets.only(top: 15.0, left: 0.0),
-                child: TextField(
-                  controller: _caregiverPin,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: 'Caregiver Pin',
-                    labelStyle: TextStyle(
-                        fontSize: TextSizeConstants.getadaptiveTextSize(
-                            context, TextSizeConstants.formField)),
-                    suffixIcon: IconButton(
-                      icon: const Icon(Icons.info_outlined),
-                      iconSize: TextSizeConstants.getadaptiveTextSize(
-                          context, TextSizeConstants.formField),
-                      onPressed: () {
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) =>
-                                _buildCaregiverPinPopop(context));
-                      },
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 15.0, left: 0.0),
-                height: 68,
-                width: 0.5 * deviceWidth,
-                child: TextButton(
+                Container(
+                  width: 0.8 * deviceWidth,
+                  margin: const EdgeInsets.only(top: 50.0, left: 0.0),
                   child: Text(
                     'Sign Up',
                     style: TextStyle(
                       fontSize: TextSizeConstants.getadaptiveTextSize(
-                          context, TextSizeConstants.buttonText),
+                          context, TextSizeConstants.memoryTitle),
                       fontFamily: 'Roboto',
-                      fontWeight: FontWeight.w400,
+                      fontWeight: FontWeight.w700,
                     ),
                   ),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        ColorConstants.buttonColor),
-                    foregroundColor: MaterialStateProperty.all<Color>(
-                        ColorConstants.buttonText),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
+                ),
+                Container(
+                  width: 0.8 * deviceWidth,
+                  margin: const EdgeInsets.only(top: 15.0, left: 0.0),
+                  child: TextButton(
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'Have an account? ',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: TextSizeConstants.getadaptiveTextSize(
+                              context, TextSizeConstants.bodyText),
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w400,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Log in.',
+                            style: TextStyle(
+                              color: ColorConstants.bodyText,
+                              fontSize: TextSizeConstants.getadaptiveTextSize(
+                                  context, TextSizeConstants.bodyText),
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      alignment: Alignment.centerLeft,
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, RouteConstants.loginRoute);
+                    },
+                  ),
+                ),
+                Container(
+                  width: 0.8 * deviceWidth,
+                  margin: const EdgeInsets.only(top: 10.0, left: 0.0),
+                  child: Text(
+                    signUpResult,
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: TextSizeConstants.getadaptiveTextSize(
+                          context, TextSizeConstants.hint),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 0.8 * deviceWidth,
+                  margin: const EdgeInsets.only(top: 15.0, left: 0.0),
+                  child: TextFormField(
+                    controller: _email,
+                    validator: AuthenticationService().validateEmail,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                          fontSize: TextSizeConstants.getadaptiveTextSize(
+                              context, TextSizeConstants.formField)),
+                      hintText: 'example@example.com',
+                      hintStyle: TextStyle(
+                          fontSize: TextSizeConstants.getadaptiveTextSize(
+                              context, TextSizeConstants.formField)),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 0.8 * deviceWidth,
+                  margin: const EdgeInsets.only(top: 15.0, left: 0.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    controller: _password,
+                    validator: AuthenticationService().validatePassword,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                          fontSize: TextSizeConstants.getadaptiveTextSize(
+                              context, TextSizeConstants.formField)),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 0.8 * deviceWidth,
+                  margin: const EdgeInsets.only(top: 15.0, left: 0.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    controller: _confirmPassword,
+                    validator: validateConfirmPassword,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Confirm Password',
+                      labelStyle: TextStyle(
+                          fontSize: TextSizeConstants.getadaptiveTextSize(
+                              context, TextSizeConstants.formField)),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 0.8 * deviceWidth,
+                  margin: const EdgeInsets.only(top: 15.0, left: 0.0),
+                  child: TextFormField(
+                    controller: _caregiverPin,
+                    obscureText: true,
+                    validator: AuthenticationService().validatePin,
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      labelText: 'Caregiver Pin',
+                      labelStyle: TextStyle(
+                          fontSize: TextSizeConstants.getadaptiveTextSize(
+                              context, TextSizeConstants.formField)),
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.info_outlined),
+                        iconSize: TextSizeConstants.getadaptiveTextSize(
+                            context, TextSizeConstants.formField),
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) =>
+                                  _buildCaregiverPinPopop(context));
+                        },
                       ),
                     ),
                   ),
-                  onPressed: () async {
-                    // final result =
-                    // await context.read<AuthenticationService>().signUp(
-                    //       email: _email.text.trim(),
-                    //       password: _password.text.trim(),
-                    //   );
-                    //     await FirebaseAuth.instance
-                    //         .createUserWithEmailAndPassword(
-                    //   email: _email.text,
-                    //   password: _password.text,
-                    // );
-                    await AuthenticationService()
-                        .signUp(
-                            email: _email.text,
-                            password: _password.text,
-                            confirmPassword: _confirmPassword.text,
-                            caregiverPin: _caregiverPin.text)
-                        .then((String result) {
-                      setState(() {
-                        signUpResult = result;
-                      });
-                    });
-                    print(signUpResult);
-                    if (signUpResult == "Success") {
-                      Navigator.pushNamed(context, RouteConstants.loginRoute);
-                    }
-                  },
                 ),
-              ),
-            ],
+                Container(
+                  margin: const EdgeInsets.only(top: 15.0, left: 0.0),
+                  height: 68,
+                  width: 0.5 * deviceWidth,
+                  child: TextButton(
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        fontSize: TextSizeConstants.getadaptiveTextSize(
+                            context, TextSizeConstants.buttonText),
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          ColorConstants.buttonColor),
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                          ColorConstants.buttonText),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                    ),
+                    onPressed: () async {
+                      // final result =
+                      // await context.read<AuthenticationService>().signUp(
+                      //       email: _email.text.trim(),
+                      //       password: _password.text.trim(),
+                      //   );
+                      //     await FirebaseAuth.instance
+                      //         .createUserWithEmailAndPassword(
+                      //   email: _email.text,
+                      //   password: _password.text,
+                      // );
+                      if (_key.currentState!.validate()) {
+                        await AuthenticationService()
+                            .signUp(
+                                email: _email.text,
+                                password: _password.text,
+                                confirmPassword: _confirmPassword.text,
+                                caregiverPin: _caregiverPin.text)
+                            .then((String result) {
+                          setState(() {
+                            signUpResult = result;
+                          });
+                        });
+                      }
+                      print(signUpResult);
+                      if (signUpResult == "Success") {
+                        Navigator.pushNamed(context, RouteConstants.loginRoute);
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
