004888e748392cc41b1319f38a10b3d6d6026794
Leena Domadia <leena.domadia2018@gmail.com>
2022-01-11 12:09:03 -0500

    changes to signup

--- lib/firebase/authentication_service.dart
+++ lib/firebase/authentication_service.dart
@@ -28,14 +28,7 @@ class AuthenticationService {
       required String password,
       required String confirmPassword,
       required String caregiverPin}) async {
-    RegExp fourDigitCode = RegExp(r"(^\d{4}$)");
     try {
-      if (password != confirmPassword) {
-        return "Passwords do not match.";
-      }
-      if (!fourDigitCode.hasMatch(caregiverPin)) {
-        return "Caregiver Pin must be a 4 digit code.";
-      }
       await _firebaseAuth.createUserWithEmailAndPassword(
           email: email, password: password);
       await FirestoreService()
@@ -44,6 +37,45 @@ class AuthenticationService {
     } on FirebaseAuthException catch (ex) {
       return ex.message.toString();
     }
+  }
+
+  String? validateEmail(String? email) {
+    if (email == null || email.isEmpty) {
+      return 'E-mail address is required.';
+    }
+
+    String pattern = r'\w+@\w+\.\w+';
+    RegExp regex = RegExp(pattern);
+    if (!regex.hasMatch(email)) {
+      return 'Invalid E-mail Address format.';
+    }
+    return null;
+  }
+
+  String? validatePassword(String? password) {
+    if (password == null || password.isEmpty) {
+      return 'Password is required.';
+    }
+
+    String pattern =
+        r'^(?=.*[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+    RegExp regex = RegExp(pattern);
+    if (!regex.hasMatch(password)) {
+      return 'Password must be at least 8 characters, include an uppercase letter, number, and symbol.';
+    }
+
+    return null;
+  }
+
+  String? validatePin(String? caregiverPin) {
+    if (caregiverPin == null || caregiverPin.isEmpty) {
+      return 'Caregiver Pin is required.';
+    }
+    RegExp fourDigitCode = RegExp(r"(^\d{4}$)");
+    if (!fourDigitCode.hasMatch(caregiverPin)) {
+      return "Caregiver Pin must be a 4 digit code.";
+    }
+    return null;
   }
 
   //SIGN OUT METHOD
