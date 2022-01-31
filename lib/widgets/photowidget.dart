fcc712365f943d46a2727068120c762af1c7ba6a
Leena Domadia <leena.domadia2018@gmail.com>
2022-01-31 12:31:37 -0500

    fixed photo display

--- lib/widgets/photowidget.dart
+++ lib/widgets/photowidget.dart
@@ -10,16 +10,26 @@ class PhotoWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                SizedBox(height:0.4*TextSizeConstants.getadaptiveTextSize(context, TextSizeConstants.bodyText)),
-                Text(description, style: TextStyle(color: ColorConstants.bodyText, fontSize: 0.9*TextSizeConstants.getadaptiveTextSize(context, TextSizeConstants.bodyText)),),
-                AspectRatio(aspectRatio: 5/4,
-                child:Image.asset(asset, fit: BoxFit.cover,),
-                )
-              ],
-            );
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        SizedBox(
+          height: 0.4 *
+              TextSizeConstants.getadaptiveTextSize(
+                  context, TextSizeConstants.bodyText),
+        ),
+        Text(
+          description,
+          style: TextStyle(
+            color: ColorConstants.bodyText,
+            fontSize: 0.9 *
+                TextSizeConstants.getadaptiveTextSize(
+                    context, TextSizeConstants.bodyText),
+          ),
+        ),
+        AspectRatio(
+            aspectRatio: 5 / 4, child: Image.network(asset, fit: BoxFit.cover))
+      ],
+    );
   }
-  
 }
