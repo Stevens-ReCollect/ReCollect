396f38105c60431235847b9522df4258e9d9c0ee
sboamah <sboamah@stevens.edu>
2022-03-06 23:29:09 -0500

    Attempting redesign

--- lib/widgets/audioplayer.dart
+++ lib/widgets/audioplayer.dart
@@ -16,6 +16,10 @@ class AudioPlayerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AudioPlayer audioPlayer = AudioPlayer();
+    format(Duration d) => d.toString().split('.').first.padLeft(8, "0");
+    final Future duration = audioPlayer.getDuration();
+    final Future currentPos = audioPlayer.getCurrentPosition();
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
@@ -39,13 +43,14 @@ class AudioPlayerWidget extends StatelessWidget {
                 TextSizeConstants.getadaptiveTextSize(
                     context, TextSizeConstants.bodyText)),
         Container(
-            width: 0.8 * MediaQuery.of(context).size.width,
+            width: 0.9 * MediaQuery.of(context).size.width,
             padding: const EdgeInsets.all(10),
             decoration: BoxDecoration(
                 color: Colors.white,
                 border:
                     Border.all(width: 1, color: ColorConstants.buttonColor)),
-            child: Row(
+            child: Column(children: [
+              Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
                 SizedBox(
@@ -56,29 +61,70 @@ class AudioPlayerWidget extends StatelessWidget {
                               TextSizeConstants.getadaptiveTextSize(
                                   context, TextSizeConstants.bodyText))),
                 ),
-                IconButton(
-                    hoverColor: ColorConstants.appBar,
-                    iconSize: TextSizeConstants.getadaptiveTextSize(
-                        context, TextSizeConstants.bodyText),
-                    onPressed: () async {
-                      await audioPlayer.setUrl(asset);
+                
+                TextButton.icon(onPressed: () async { 
+                  await audioPlayer.setUrl(asset);
                       audioPlayer.play(asset);
-                      print('playing now');
-                    },
-                    icon: Icon(Icons.play_arrow,
-                        color: ColorConstants.buttonColor)),
-                IconButton(
-                    hoverColor: ColorConstants.appBar,
-                    highlightColor: Colors.black,
-                    iconSize: TextSizeConstants.getadaptiveTextSize(
-                        context, TextSizeConstants.bodyText),
-                    onPressed: () {
+                      print('playing');
+                    }, 
+                    icon: const Icon(Icons.play_arrow), 
+                    label: Text('Play',
+                      style: TextStyle(
+                          fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+                                  context, TextSizeConstants.bodyText))),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                      foregroundColor: MaterialStateProperty.all<Color>(ColorConstants.buttonColor),
+                      ),
+                  ),
+                  TextButton.icon(onPressed: () async { 
+                  await audioPlayer.setUrl(asset);
                       audioPlayer.pause();
                       print('paused');
-                    },
-                    icon: Icon(Icons.pause, color: ColorConstants.buttonColor))
+                    }, 
+                    icon: const Icon(Icons.pause), 
+                    label: Text('Pause',
+                      style: TextStyle(
+                          fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
+                                  context, TextSizeConstants.bodyText))),
+                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                      foregroundColor: MaterialStateProperty.all<Color>(ColorConstants.buttonColor),),),
+                    
               ],
-            )),
+            ),
+            const SizedBox(height: 20,),
+            FutureBuilder<List<dynamic>>(
+              future: Future.wait([currentPos, duration]),
+              builder: (context, snapshot) {
+                var myList = snapshot.data!; 
+                   switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                      return const Text('No Connection...');
+                    case ConnectionState.active:
+                    case ConnectionState.waiting:
+                      return const Text('Awaiting result...');
+                    case ConnectionState.done:
+                      if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                  }
+                return Row(children: [
+                Text(format(Duration(seconds:myList[0])).toString() + '/' + format(Duration(seconds:myList[1])).toString().padLeft(8, "0")),
+                Slider.adaptive(
+                  value: double.parse(myList[0].toString()), 
+                  max: double.parse(myList[1].toString()),
+                  activeColor: ColorConstants.buttonColor,
+                  onChanged: (value) async {
+                    await audioPlayer.setUrl(asset);
+                    myList[0] = value;
+                   })
+                ]);
+                 }
+            
+              }
+
+            ),
+            ])),
+
           AffirmButtonsWidget(doc_id).build(context),
       ],
     );
