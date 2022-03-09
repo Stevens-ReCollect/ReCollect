7cd8143cfccce41a1553fe263b55f345fe703ad6
Kushal Patel <kushal1223@gmail.com>
2022-03-09 15:41:08 -0500

    Loading indicator fix

--- lib/addaudio.dart
+++ lib/addaudio.dart
@@ -90,141 +90,149 @@ class _AddAudioPageState extends State<AddAudioPage> {
     MediaQueryData queryData = MediaQuery.of(context); //responsive sizing
     var deviceWidth = queryData.size.width;
     var deviceHeight = queryData.size.height;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios_new),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        title: const Text("Add Audio"),
-      ),
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              contentPadding:
-                  EdgeInsets.symmetric(horizontal: 0.1 * deviceWidth),
-              title: Text(
-                audioName,
-                style: const TextStyle(
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w400,
-                    fontSize: 18.0),
-              ),
-              subtitle: TextButton(
-                onPressed: () {
-                  pickAudio();
-                },
-                child: const Text(
-                  'Change Audio',
-                  style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontWeight: FontWeight.w400,
-                      fontSize: 12.0),
-                ),
-                style: TextButton.styleFrom(
-                  minimumSize: Size.zero,
-                  padding: EdgeInsets.zero,
-                  alignment: Alignment.centerLeft,
-                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                ),
-              ),
-              leading: ClipRRect(
-                borderRadius: BorderRadius.circular(5.0),
-                child: ConstrainedBox(
-                  constraints: const BoxConstraints(
-                    minHeight: 60.0,
-                    minWidth: 60.0,
-                    maxHeight: 60.0,
-                    maxWidth: 60.0,
-                  ),
-                  child: Image.asset(
-                    'lib/assets/audioicon.png',
-                    width: 60.0,
-                    height: 60.0,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
+    return Container(
+        child: Stack(children: <Widget>[
+      Container(
+          child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            elevation: 0,
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios_new),
+              onPressed: () {
+                Navigator.pop(context);
+              },
             ),
-            Container(
-              height: 0.3 * deviceHeight,
-              width: 0.8 * deviceWidth,
-              margin: const EdgeInsets.only(top: 15.0, left: 0.0),
-              child: TextField(
-                controller: _description,
-                maxLines: 15,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  labelText: 'Description',
-                  labelStyle: TextStyle(
-                      fontSize: TextSizeConstants.getadaptiveTextSize(
-                          context, TextSizeConstants.formField)),
-                  hintText: 'Description',
-                  hintStyle: TextStyle(
-                      fontSize: TextSizeConstants.getadaptiveTextSize(
-                          context, TextSizeConstants.formField)),
-                  alignLabelWithHint: true,
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 150.0, left: 0.0),
-              width: 0.4 * deviceWidth,
-              child: TextButton(
-                child: Text(
-                  'Save',
-                  style: TextStyle(
-                    fontSize: TextSizeConstants.getadaptiveTextSize(
-                        context, TextSizeConstants.buttonText),
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w400,
+            title: const Text("Add Audio"),
+          ),
+          backgroundColor: Colors.white,
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 0.1 * deviceWidth),
+                  title: Text(
+                    audioName,
+                    style: const TextStyle(
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 18.0),
                   ),
-                ),
-                style: ButtonStyle(
-                  padding: MaterialStateProperty.all(const EdgeInsets.all(10)),
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      ColorConstants.buttonColor),
-                  foregroundColor: MaterialStateProperty.all<Color>(
-                      ColorConstants.buttonText),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
+                  subtitle: TextButton(
+                    onPressed: () {
+                      pickAudio();
+                    },
+                    child: const Text(
+                      'Change Audio',
+                      style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12.0),
+                    ),
+                    style: TextButton.styleFrom(
+                      minimumSize: Size.zero,
+                      padding: EdgeInsets.zero,
+                      alignment: Alignment.centerLeft,
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                  ),
+                  leading: ClipRRect(
+                    borderRadius: BorderRadius.circular(5.0),
+                    child: ConstrainedBox(
+                      constraints: const BoxConstraints(
+                        minHeight: 60.0,
+                        minWidth: 60.0,
+                        maxHeight: 60.0,
+                        maxWidth: 60.0,
+                      ),
+                      child: Image.asset(
+                        'lib/assets/audioicon.png',
+                        width: 60.0,
+                        height: 60.0,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
                 ),
-                onPressed: _isButtonDisabled
-                    ? null
-                    : () async {
-                        setState(() {
-                          _loading = true;
-                          _isButtonDisabled = true;
-                        });
-                        print("Clicked Saved");
-                        if (audio != null) {
-                          await FirestoreService().addNewMoment(
-                              memoryId: widget.memoryData['doc_id'],
-                              type: 'Audio',
-                              file: audio,
-                              name: audioName,
-                              thumbnail: thumbnail,
-                              description: _description.text);
-                        }
-                        // Navigator.pushNamed(
-                        //     context, RouteConstants.memoryHomeRoute);
-                        Navigator.pop(context);
-                      },
-              ),
+                Container(
+                  height: 0.3 * deviceHeight,
+                  width: 0.8 * deviceWidth,
+                  margin: const EdgeInsets.only(top: 15.0, left: 0.0),
+                  child: TextField(
+                    controller: _description,
+                    maxLines: 15,
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      labelText: 'Description',
+                      labelStyle: TextStyle(
+                          fontSize: TextSizeConstants.getadaptiveTextSize(
+                              context, TextSizeConstants.formField)),
+                      hintText: 'Description',
+                      hintStyle: TextStyle(
+                          fontSize: TextSizeConstants.getadaptiveTextSize(
+                              context, TextSizeConstants.formField)),
+                      alignLabelWithHint: true,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 150.0, left: 0.0),
+                  width: 0.4 * deviceWidth,
+                  child: TextButton(
+                    child: Text(
+                      'Save',
+                      style: TextStyle(
+                        fontSize: TextSizeConstants.getadaptiveTextSize(
+                            context, TextSizeConstants.buttonText),
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    style: ButtonStyle(
+                      padding:
+                          MaterialStateProperty.all(const EdgeInsets.all(10)),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          ColorConstants.buttonColor),
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                          ColorConstants.buttonText),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                    ),
+                    onPressed: _isButtonDisabled
+                        ? null
+                        : () async {
+                            setState(() {
+                              _loading = true;
+                              _isButtonDisabled = true;
+                            });
+                            print("Clicked Saved");
+                            if (audio != null) {
+                              await FirestoreService().addNewMoment(
+                                  memoryId: widget.memoryData['doc_id'],
+                                  type: 'Audio',
+                                  file: audio,
+                                  name: audioName,
+                                  thumbnail: thumbnail,
+                                  description: _description.text);
+                            }
+                            // Navigator.pushNamed(
+                            //     context, RouteConstants.memoryHomeRoute);
+                            Navigator.pop(context);
+                          },
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
-    );
+      )),
+      Container(alignment: Alignment.center, child: loading())
+    ]));
   }
 }
