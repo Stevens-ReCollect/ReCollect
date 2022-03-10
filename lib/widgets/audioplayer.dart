777501027ae93d42c6bb40191a9ca6ce270f0ae5
Sylvia Boamah <60356081+sboamah@users.noreply.github.com>
2022-03-10 14:43:35 -0500

    Some changes

--- lib/widgets/audioplayer.dart
+++ lib/widgets/audioplayer.dart
@@ -5,20 +5,32 @@ import 'package:recollect_app/constants/colorConstants.dart';
 import 'package:recollect_app/constants/textSizeConstants.dart';
 import 'package:recollect_app/widgets/affirmButtons.dart';
 
-class AudioPlayerWidget extends StatelessWidget {
-  const AudioPlayerWidget(
-      this.description, this.title, this.asset, this.doc_id);
+class AudioPlayerWidget extends StatefulWidget {
+  const AudioPlayerWidget(this.description, this.title, this.asset, this.doc_id, {Key? key}) : super(key: key);
+  
   final description;
   final title;
   final asset;
   final doc_id;
 
   @override
+  AudioPlayerState createState() => AudioPlayerState();
+}
+
+class AudioPlayerState extends State <AudioPlayerWidget> {
+
+  @override
   Widget build(BuildContext context) {
-    AudioPlayer audioPlayer = AudioPlayer();
-    format(Duration d) => d.toString().split('.').first.padLeft(8, "0");
+    AudioPlayer audioPlayer = AudioPlayer(mode: PlayerMode.MEDIA_PLAYER);
     final Future duration = audioPlayer.getDuration();
     final Future currentPos = audioPlayer.getCurrentPosition();
+
+    initState(){
+      audioPlayer.setUrl(widget.asset);
+    }
+
+    format(Duration d) => d.toString().split('.').first.padLeft(8, "0"); //duration format
+    
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -30,7 +42,7 @@ class AudioPlayerWidget extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.all(10),
           child: Text(
-            description,
+            widget.description,
             style: TextStyle(
                 color: ColorConstants.bodyText,
                 fontSize: 0.9 *
@@ -55,7 +67,7 @@ class AudioPlayerWidget extends StatelessWidget {
               children: <Widget>[
                 SizedBox(
                   width: 0.4 * MediaQuery.of(context).size.width,
-                  child: Text(title,
+                  child: Text(widget.title,
                       style: TextStyle(
                           fontSize: 0.7 *
                               TextSizeConstants.getadaptiveTextSize(
@@ -63,8 +75,8 @@ class AudioPlayerWidget extends StatelessWidget {
                 ),
                 
                 TextButton.icon(onPressed: () async { 
-                  await audioPlayer.setUrl(asset);
-                      audioPlayer.play(asset);
+                  await audioPlayer.setUrl(widget.asset);
+                      audioPlayer.play(widget.asset);
                       print('playing');
                     }, 
                     icon: const Icon(Icons.play_arrow), 
@@ -78,7 +90,7 @@ class AudioPlayerWidget extends StatelessWidget {
                       ),
                   ),
                   TextButton.icon(onPressed: () async { 
-                  await audioPlayer.setUrl(asset);
+                  await audioPlayer.setUrl(widget.asset);
                       audioPlayer.pause();
                       print('paused');
                     }, 
@@ -93,39 +105,48 @@ class AudioPlayerWidget extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 20,),
-            FutureBuilder<List<dynamic>>(
-              future: Future.wait([currentPos, duration]),
-              builder: (context, snapshot) {
-                var myList = snapshot.data!; 
-                   switch (snapshot.connectionState) {
+            FutureBuilder<dynamic>(
+              future: currentPos,
+              builder: (context, snapshot1) {
+                switch (snapshot1.connectionState) {
                     case ConnectionState.none:
                       return const Text('No Connection...');
                     case ConnectionState.active:
                     case ConnectionState.waiting:
                       return const Text('Awaiting result...');
                     case ConnectionState.done:
-                      if (snapshot.hasError) {
-                        return Text('Error: ${snapshot.error}');
-                  }
+                      if (snapshot1.hasError) {
+                        return Text('Error: ${snapshot1.error}');
+                  } else{
+                return FutureBuilder<dynamic>(
+                future: duration,
+                builder: (context, snapshot2) {
+                   switch (snapshot2.connectionState) {
+                    case ConnectionState.none:
+                      return const Text('No Connection...');
+                    case ConnectionState.active:
+                    case ConnectionState.waiting:
+                      return const Text('Awaiting result...');
+                    case ConnectionState.done:
+                      if (snapshot2.hasError) {
+                        return Text('Error: ${snapshot2.error}');
+                  } else{
                 return Row(children: [
-                Text(format(Duration(seconds:myList[0])).toString() + '/' + format(Duration(seconds:myList[1])).toString().padLeft(8, "0")),
+                Text(format(Duration(seconds:snapshot1.data)) + '/' + format(Duration(seconds:snapshot2.data)).padLeft(8, "0")),
                 Slider.adaptive(
-                  value: double.parse(myList[0].toString()), 
-                  max: double.parse(myList[1].toString()),
+                  value: double.parse(snapshot1.data.toString()), 
+                  max: double.parse(snapshot2.data.toString()),
                   activeColor: ColorConstants.buttonColor,
                   onChanged: (value) async {
-                    await audioPlayer.setUrl(asset);
-                    myList[0] = value;
+                    await audioPlayer.setUrl(widget.asset);
                    })
                 ]);
                  }
-            
               }
+              });}
+                }})])),
 
-            ),
-            ])),
-
-          AffirmButtonsWidget(doc_id).build(context),
+          AffirmButtonsWidget(widget.doc_id).build(context),
       ],
     );
   }
