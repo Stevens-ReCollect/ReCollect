a22da14e34bd52e0be12c5256ffc7e750a01d2a5
Leena Domadia <leena.domadia2018@gmail.com>
2022-03-10 15:07:08 -0500

    update font size

--- lib/widgets/audioplayer.dart
+++ lib/widgets/audioplayer.dart
@@ -6,8 +6,10 @@ import 'package:recollect_app/constants/textSizeConstants.dart';
 import 'package:recollect_app/widgets/affirmButtons.dart';
 
 class AudioPlayerWidget extends StatefulWidget {
-  const AudioPlayerWidget(this.description, this.title, this.asset, this.doc_id, {Key? key}) : super(key: key);
-  
+  const AudioPlayerWidget(this.description, this.title, this.asset, this.doc_id,
+      {Key? key})
+      : super(key: key);
+
   final description;
   final title;
   final asset;
@@ -17,20 +19,19 @@ class AudioPlayerWidget extends StatefulWidget {
   AudioPlayerState createState() => AudioPlayerState();
 }
 
-class AudioPlayerState extends State <AudioPlayerWidget> {
-
+class AudioPlayerState extends State<AudioPlayerWidget> {
   @override
   Widget build(BuildContext context) {
     AudioPlayer audioPlayer = AudioPlayer(mode: PlayerMode.MEDIA_PLAYER);
-    final Future duration = audioPlayer.getDuration();
-    final Future currentPos = audioPlayer.getCurrentPosition();
+    Future duration = audioPlayer.getDuration();
+    Future currentPos = audioPlayer.getCurrentPosition();
 
-    initState(){
+    initState() {
       audioPlayer.setUrl(widget.asset);
     }
 
-    format(Duration d) => d.toString().split('.').first.padLeft(8, "0"); //duration format
-    
+    format(Duration d) =>
+        d.toString().split('.').first.padLeft(8, "0"); //duration format
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -63,90 +64,113 @@ class AudioPlayerState extends State <AudioPlayerWidget> {
                     Border.all(width: 1, color: ColorConstants.buttonColor)),
             child: Column(children: [
               Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                SizedBox(
-                  width: 0.4 * MediaQuery.of(context).size.width,
-                  child: Text(widget.title,
-                      style: TextStyle(
-                          fontSize: 0.7 *
-                              TextSizeConstants.getadaptiveTextSize(
-                                  context, TextSizeConstants.bodyText))),
-                ),
-                
-                TextButton.icon(onPressed: () async { 
-                  await audioPlayer.setUrl(widget.asset);
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  SizedBox(
+                    width: 0.4 * MediaQuery.of(context).size.width,
+                    child: Text(widget.title,
+                        style: TextStyle(
+                            fontSize: 0.7 *
+                                TextSizeConstants.getadaptiveTextSize(
+                                    context, TextSizeConstants.bodyText))),
+                  ),
+                  TextButton.icon(
+                    onPressed: () async {
+                      await audioPlayer.setUrl(widget.asset);
                       audioPlayer.play(widget.asset);
                       print('playing');
-                    }, 
-                    icon: const Icon(Icons.play_arrow), 
+                    },
+                    icon: const Icon(Icons.play_arrow),
                     label: Text('Play',
-                      style: TextStyle(
-                          fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
-                                  context, TextSizeConstants.bodyText))),
+                        style: TextStyle(
+                            fontSize: 0.6 *
+                                TextSizeConstants.getadaptiveTextSize(
+                                    context, TextSizeConstants.bodyText))),
                     style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                      foregroundColor: MaterialStateProperty.all<Color>(ColorConstants.buttonColor),
-                      ),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                          ColorConstants.buttonColor),
+                    ),
                   ),
-                  TextButton.icon(onPressed: () async { 
-                  await audioPlayer.setUrl(widget.asset);
+                  TextButton.icon(
+                    onPressed: () async {
+                      await audioPlayer.setUrl(widget.asset);
                       audioPlayer.pause();
                       print('paused');
-                    }, 
-                    icon: const Icon(Icons.pause), 
+                    },
+                    icon: const Icon(Icons.pause),
                     label: Text('Pause',
-                      style: TextStyle(
-                          fontSize: 0.7*TextSizeConstants.getadaptiveTextSize(
-                                  context, TextSizeConstants.bodyText))),
-                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                      foregroundColor: MaterialStateProperty.all<Color>(ColorConstants.buttonColor),),),
-                    
-              ],
-            ),
-            const SizedBox(height: 20,),
-            FutureBuilder<dynamic>( // to get current position and duration
-              future: currentPos,
-              builder: (context, snapshot1) {
-                switch (snapshot1.connectionState) {
-                    case ConnectionState.none:
-                      return const Text('No Connection...');
-                    case ConnectionState.active:
-                    case ConnectionState.waiting:
-                      return const Text('Awaiting result...');
-                    case ConnectionState.done:
-                      if (snapshot1.hasError) {
-                        return Text('Error: ${snapshot1.error}');
-                  } else{
-                return FutureBuilder<dynamic>(
-                future: duration,
-                builder: (context, snapshot2) {
-                   switch (snapshot2.connectionState) {
-                    case ConnectionState.none:
-                      return const Text('No Connection...');
-                    case ConnectionState.active:
-                    case ConnectionState.waiting:
-                      return const Text('Awaiting result...');
-                    case ConnectionState.done:
-                      if (snapshot2.hasError) {
-                        return Text('Error: ${snapshot2.error}');
-                  } else{
-                return Row(children: [
-                Text(format(Duration(seconds:snapshot1.data)) + '/' + format(Duration(seconds:snapshot2.data)).padLeft(8, "0")),
-                Slider.adaptive(
-                  value: double.parse(snapshot1.data.toString()), 
-                  max: double.parse(snapshot2.data.toString()),
-                  activeColor: ColorConstants.buttonColor,
-                  onChanged: (value) async {
-                    await audioPlayer.setUrl(widget.asset);
-                   })
-                ]);
-                 }
-              }
-              });}
-                }})])),
-
-          AffirmButtonsWidget(widget.doc_id).build(context),
+                        style: TextStyle(
+                            fontSize: 0.6 *
+                                TextSizeConstants.getadaptiveTextSize(
+                                    context, TextSizeConstants.bodyText))),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                          ColorConstants.buttonColor),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              FutureBuilder<dynamic>(
+                  // to get current position and duration
+                  future: currentPos,
+                  builder: (context, snapshot1) {
+                    switch (snapshot1.connectionState) {
+                      case ConnectionState.none:
+                        return const Text('No Connection...');
+                      case ConnectionState.active:
+                      case ConnectionState.waiting:
+                        return const Text('Awaiting result...');
+                      case ConnectionState.done:
+                        if (snapshot1.hasError) {
+                          return Text('Error: ${snapshot1.error}');
+                        } else {
+                          return FutureBuilder<dynamic>(
+                              future: duration,
+                              builder: (context, snapshot2) {
+                                switch (snapshot2.connectionState) {
+                                  case ConnectionState.none:
+                                    return const Text('No Connection...');
+                                  case ConnectionState.active:
+                                  case ConnectionState.waiting:
+                                    return const Text('Awaiting result...');
+                                  case ConnectionState.done:
+                                    if (snapshot2.hasError) {
+                                      return Text('Error: ${snapshot2.error}');
+                                    } else {
+                                      return Row(children: [
+                                        Text(format(Duration(
+                                                seconds: snapshot1.data)) +
+                                            '/' +
+                                            format(Duration(
+                                                    seconds: snapshot2.data))
+                                                .padLeft(8, "0")),
+                                        // Slider.adaptive(
+                                        //     value: double.parse(
+                                        //         snapshot1.data.toString()),
+                                        //     max: double.parse(
+                                        //         snapshot2.data.toString()),
+                                        //     activeColor:
+                                        //         ColorConstants.buttonColor,
+                                        //     onChanged: (value) async {
+                                        //       await audioPlayer
+                                        //           .setUrl(widget.asset);
+                                        //     })
+                                      ]);
+                                    }
+                                }
+                              });
+                        }
+                    }
+                  })
+            ])),
+        AffirmButtonsWidget(widget.doc_id).build(context),
       ],
     );
   }
