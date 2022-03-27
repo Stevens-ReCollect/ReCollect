3af0deaf09c1defdab201f5f9f16d9c294225fd6
Leena Domadia <leena.domadia2018@gmail.com>
2022-03-27 13:18:59 -0400

    changed format of date

--- lib/memorydetails.dart
+++ lib/memorydetails.dart
@@ -13,42 +13,73 @@ class MemoryDetailsPage extends StatelessWidget {
     var deviceHeight = queryData.size.height;
 
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-          elevation: 0,
-        ),
-        body: Column(
-          children: [
-            Stack(
-              children: [
-                SizedBox(
-                  height: deviceHeight * 0.3,
-                  width: deviceWidth,
-                  child: Image(
-                    image: NetworkImage(memoryData['file_path']),
-                    alignment: Alignment.center,
-                    fit: BoxFit.cover,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              SizedBox(
+                height: deviceHeight * 0.3,
+                width: deviceWidth,
+                child: Image(
+                  image: NetworkImage(memoryData['file_path']),
+                  alignment: Alignment.center,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                height: deviceHeight * 0.3,
+                width: deviceWidth,
+                alignment: Alignment.bottomLeft,
+                padding: const EdgeInsets.only(left: 20, bottom: 10),
+                child: Text(
+                  memoryData['title'],
+                  style: TextStyle(
+                    color: ColorConstants.buttonText,
+                    fontSize: TextSizeConstants.getadaptiveTextSize(
+                        context, TextSizeConstants.h2),
+                    fontWeight: FontWeight.w900,
                   ),
                 ),
-                Container(
-                  height: deviceHeight * 0.3,
-                  width: deviceWidth,
-                  alignment: Alignment.bottomLeft,
-                  padding: const EdgeInsets.only(left: 20, bottom: 10),
-                  child: Text(
-                    memoryData['title'],
-                    style: TextStyle(
-                      color: ColorConstants.buttonText,
-                      fontSize: TextSizeConstants.getadaptiveTextSize(
-                          context, TextSizeConstants.h2),
-                      fontWeight: FontWeight.w900,
-                    ),
-                  ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.only(
+                    left: 20, bottom: 10, top: 10, right: 10),
+                child: Icon(
+                  Icons.calendar_today_rounded,
+                  color: Colors.grey,
+                  size: TextSizeConstants.getadaptiveTextSize(
+                      context, TextSizeConstants.h2),
                 ),
-              ],
-            )
-          ],
-        ));
+              ),
+              Text(
+                memoryData['start_date'],
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: TextSizeConstants.getadaptiveTextSize(
+                      context, TextSizeConstants.bodyText),
+                ),
+              ),
+              Text(
+                " - " + memoryData['end_date'],
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: TextSizeConstants.getadaptiveTextSize(
+                      context, TextSizeConstants.bodyText),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
