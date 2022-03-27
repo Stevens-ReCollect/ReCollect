440aacde8626bfddaaada5876cb0dc7b62672483
Leena Domadia <leena.domadia2018@gmail.com>
2022-03-27 12:03:51 -0400

    fixed load

--- lib/memory.dart
+++ lib/memory.dart
@@ -25,109 +25,19 @@ class _MemoryState extends State<MemoryPage> {
 
   @override
   void initState() {
-    getMoments();
     super.initState();
-  }
-
-  void updatePostiton(double position) {
-    setState(() {
-      _currentPage = position.toInt();
+    getMomentsFromFirebase().then((data) {
+      setState(() {
+        moments = data;
+        numOfMoments = data.length;
+      });
     });
   }
 
-  getMoments() async {
-    moments = await FirestoreService()
+  Future getMomentsFromFirebase() async {
+    var data = await FirestoreService()
         .getMoments(memoryId: widget.memoryData['doc_id']);
-    // print(moments);
-    // print(moments.length);
-    numOfMoments = moments.length;
-  }
-
-  memoryCarouselSlider() {
-    MediaQueryData queryData = MediaQuery.of(context);
-    var deviceHeight = queryData.size.height;
-    var deviceWidth = queryData.size.width;
-    Stream<QuerySnapshot> _momentStream = FirebaseFirestore.instance
-        .collection('moments')
-        .where("memory_id", isEqualTo: widget.memoryData['doc_id'])
-        .snapshots();
-    return StreamBuilder<QuerySnapshot>(
-        stream: _momentStream,
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return const Text('Something went wrong');
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Text('Loading...');
-          }
-          numOfMoments = snapshot.data!.docs.length;
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: deviceWidth,
-                height: 0.7 * deviceHeight,
-                child: Center(
-                  child: CarouselSlider(
-                    carouselController: _controller,
-                    options: CarouselOptions(
-                      autoPlay: false,
-                      aspectRatio: 1 / 1,
-                      height: deviceHeight,
-                      viewportFraction: 1,
-                      enableInfiniteScroll: false,
-                      onPageChanged: (index, reason) {
-                        _currentPage = index;
-                        // updatePostiton(index.toDouble());
-                        print('Current Page: $index');
-                      },
-                    ),
-                    items: snapshot.data!.docs.map((DocumentSnapshot document) {
-                      Map<String, dynamic> data =
-                          document.data()! as Map<String, dynamic>;
-                      return Builder(builder: (BuildContext context) {
-                        // print("Print data: $data");
-                        if (data.isEmpty) {
-                          return const Text("This memory is empty.");
-                        } else {
-                          if (data['type'] == 'Photo') {
-                            return PhotoWidget(data['description'],
-                                data['file_path'], data['doc_id']);
-                          } else if (data['type'] == 'Video') {
-                            return VideoPlayerWidget(data['description'],
-                                data['file_path'], data['doc_id']);
-                          } else if (data['type'] == 'Audio') {
-                            return AudioPlayerWidget(
-                                data['description'],
-                                data['name'],
-                                data['file_path'],
-                                data['doc_id']);
-                          } else {
-                            return const SizedBox();
-                          }
-                        }
-                      });
-                    }).toList(),
-                  ),
-                ),
-              ),
-              DotsIndicator(
-                dotsCount: numOfMoments,
-                position: _currentPage.toDouble(),
-                decorator: DotsDecorator(
-                  activeColor: Colors.grey[850],
-                  color: Colors.grey,
-                ),
-                onTap: (pos) {
-                  print(pos);
-                  // setState(() => _currentPage = pos.toInt());
-                },
-              ),
-            ],
-          );
-        });
+    return data;
   }
 
   @override
@@ -153,6 +63,7 @@ class _MemoryState extends State<MemoryPage> {
             width: deviceWidth,
             height: 0.7 * deviceHeight,
             child: CarouselSlider(
+              carouselController: _controller,
               options: CarouselOptions(
                 autoPlay: false,
                 aspectRatio: 1 / 1,
@@ -163,12 +74,10 @@ class _MemoryState extends State<MemoryPage> {
                   setState(() {
                     _currentPage = index;
                   });
-                  // updatePostiton(index.toDouble());
-                  print('Current Page: $index');
                 },
               ),
               items: moments.map((moment) {
-                print(moment);
+                // print(moment);
                 return Builder(builder: (BuildContext context) {
                   if (moment == null) {
                     return const Text("This memory is empty.");
@@ -207,34 +116,9 @@ class _MemoryState extends State<MemoryPage> {
               activeColor: Colors.grey[850],
               color: Colors.grey,
             ),
-            onTap: (pos) {
-              print(pos);
-              // setState(() => _currentPage = pos.toInt());
-            },
           ),
         ]),
-        // child: Column(
-        //   crossAxisAlignment: CrossAxisAlignment.center,
-        //   mainAxisSize: MainAxisSize.max,
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   children: <Widget>[
-        //     SizedBox(
-        //       width: deviceWidth,
-        //       height: 0.7 * deviceHeight,
-        //       child: Center(child: memoryCarouselSlider()),
-        //     ),
-        //     DotsIndicator(
-        //       dotsCount: numOfMoments,
-        //       position: _currentPage.toDouble(),
-        //       decorator: DotsDecorator(
-        //         activeColor: Colors.grey[850],
-        //         color: Colors.grey,
-        //       ),
-        //     )
-        //   ],
-        // ),
       ),
-      // )
     );
   }
 }
